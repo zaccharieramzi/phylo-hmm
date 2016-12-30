f07fa27c4254ec57772f71cbb91fd4b360ab733c
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-30 19:01:19 +0100

    made viterbi pep8 compliant in terms of line length

--- viterbi_sumproduct.py
+++ viterbi_sumproduct.py
@@ -5,7 +5,8 @@ def sum_log(a, axis=0):
     '''
     Sum when working with logarithms to avoid numerical errors
     :param a: vector or matrix
-    :param axis: axis along which the sum is performed, useful if 'a' is a matrix
+    :param axis: axis along which the sum is performed, useful if 'a' is a
+    matrix
     :return: m + log(sum_i exp(a_i - m)) with m = max(a)
     '''
     # 'a' is a vector
@@ -21,15 +22,18 @@ def sum_log(a, axis=0):
 
 def forward(A, b, E, mode):
     '''
-    Forward pass: computes the logarithms of alpha-messages associated to the Sum-Product
+    Forward pass: computes the logarithms of alpha-messages associated to the
+    Sum-Product
     algorithm or Viterbi. Logarithms are used to avoid numerical errors.
     :param A: matrix of state-transition probabilities (M rows, M columns)
     :param b: vector of initial-state probabilities (dimension M)
-    :param E: matrix of emission probabilities computed with Felstenstein algorithm (M rows, L columns)
-    :param mode: string to precise how to compute the log alpha-messages. Must be 'max' for Viterbi,
+    :param E: matrix of emission probabilities computed with Felstenstein
+    algorithm (M rows, L columns)
+    :param mode: string to precise how to compute the log alpha-messages. Must
+    be 'max' for Viterbi,
             'sum' for Sum-Product
-    :return: if 'max', matrix of log alpha-messages and the argmax matrix ; if 'sum', matrix of
-            log alpha-messages
+    :return: if 'max', matrix of log alpha-messages and the argmax matrix ; if
+    'sum', matrix of log alpha-messages
     '''
     if mode != 'max' and mode != 'sum':
         return "Error: Input parameter mode must be 'sum' or 'max'!"
@@ -54,9 +58,11 @@ def forward(A, b, E, mode):
 
 def backward(A, E):
     '''
-    Backward pass: computes the logarithms of beta-messages for the Sum-Product algorithm
+    Backward pass: computes the logarithms of beta-messages for the Sum-Product
+    algorithm
     :param A: matrix of state-transition probabilities (M rows, M columns)
-    :param E: matrix of emission probabilities computed with Felstenstein algorithm (M rows, L columns)
+    :param E: matrix of emission probabilities computed with Felstenstein
+    algorithm (M rows, L columns)
     :return: matrix of log beta-messages
     '''
     # Initialization
@@ -64,7 +70,8 @@ def backward(A, E):
     beta_log = np.zeros((M, L))
     # Recursion
     for t in range(L-2, -1, -1):
-        beta_log[:, t] = sum_log(np.log(A) + np.log(E[:, t+1]) + beta_log[:, t+1], axis=1)
+        beta_log[:, t] = sum_log(
+            np.log(A) + np.log(E[:, t+1]) + beta_log[:, t+1], axis=1)
     return beta_log
 
 
@@ -73,16 +80,19 @@ def sum_product(A, b, E):
     Sum-Product algorithm (forward-backward procedure)
     :param A: matrix of state-transition probabilities (M rows, M columns)
     :param b: vector of initial-state probabilities (dimension M)
-    :param E: matrix of emission probabilities computed with Felstenstein algorithm (M rows, L columns)
+    :param E: matrix of emission probabilities computed with Felstenstein
+    algorithm (M rows, L columns)
     :return: matrix of posterior probabilities
     '''
     M, L = E.shape
     post_probas = np.zeros((M, L))
-    # Forward and backward procedure to compute the logarithms of alpha and beta messages
+    # Forward and backward procedure to compute the logarithms of alpha and
+    # beta messages
     alpha_log = forward(A, b, E, 'sum')
     beta_log = backward(A, E)
     # Posterior probabilities computation using the log method
-    post_probas = np.exp(alpha_log + beta_log - sum_log(alpha_log + beta_log, axis=0))
+    post_probas = np.exp(
+        alpha_log + beta_log - sum_log(alpha_log + beta_log, axis=0))
     return post_probas
 
 
@@ -92,7 +102,8 @@ def viterbi(S, A, b, E):
     :param S: state space (line vector 1,...,M)
     :param A: matrix of state-transition probabilities (M rows, M columns)
     :param b: vector of initial-state probabilities (dimension M)
-    :param E: matrix of emission probabilities computed with Felstenstein algorithm (M rows, L columns)
+    :param E: matrix of emission probabilities computed with Felstenstein
+    algorithm (M rows, L columns)
                 (instead of E => call directly Felstenstein in this function?)
     :return: the maximum-likelihood path (vector of dimension L)
     '''
@@ -101,7 +112,7 @@ def viterbi(S, A, b, E):
     phi = np.zeros(L)
     # Recursion
     [alpha_log, argmaxAlpha] = forward(A, b, E, 'max')
-    # Decoding the argmax
+    # Decoding the argmax
     opt_sequence = np.zeros(L, dtype=int)
     opt_sequence[-1] = np.argmax(alpha_log[:, -1])
     phi[-1] = S[opt_sequence[-1]]
