d7ab17c14d5e3acd39a5b39f539efaec644e6828
tournemire <pierredet@gmail.com>
2017-01-02 15:36:50 +0100

    integrated sub_tree into model_evalulation.py

--- model_evaluation.py
+++ model_evaluation.py
@@ -9,7 +9,8 @@ from viterbi_sumproduct import sum_product, viterbi
 
 
 def single_decoding_routine(tree_path, number_of_nucleotids, alphabet, A,
-                            b, n_species, pi, kappa, alg='viterbi'):
+                            b, n_species, pi, kappa, alg='viterbi',
+                            list_of_species=[]):
     '''Generates a sequence of states and an associated list of strands. Then
     decodes those using a phylogenetic HMM model.
     '''
@@ -17,6 +18,10 @@ def single_decoding_routine(tree_path, number_of_nucleotids, alphabet, A,
     # load the phylogenetic model from JSON
     tree = load_tree(tree_path)
     trees = []
+
+    if list_of_species:
+        tree = sub_tree(tree, list_of_species)
+        n_species = len(list_of_species)
 
     for j in range(nbState):
         scaling_factor = random.random()
@@ -111,3 +116,70 @@ def specificity(reference_codon_sequence, comparison_codon_sequence):
         reference_codon_sequence, comparison_codon_sequence)
     true_negatives = negative_predictions * true_predicitons
     return np.sum(true_negatives) / np.sum(negative_predictions)
+
+
+def sub_tree(tree, list_of_species):
+    """
+    Prune the tree to keep only keep species in a list
+    Args :
+           - tree (dict)
+           - list_of_species (int list)list of index
+    """
+    tree_cp = deepcopy(tree)
+
+    def treat_node(node):
+        children = tree[node]  # important for the recursion
+        if children:  # propagate recirsively
+            for child in children:
+                treat_node(child["node"])
+
+            for child in children:
+                new_child = child["node"]
+                if new_child not in list_of_species and not tree_cp[new_child]:
+                    # we drop the child
+                    tree_cp[node].remove(child)
+                    # discard "dead leaf"
+                    tree_cp.pop(new_child, None)
+
+    treat_node(max(tree_cp.keys()))
+
+    # now merge useless intermediary node ie with only one son
+    def merge_unary(node, ancestor_list):
+        if(len(tree_cp[node]) == 1):
+            child = tree_cp[node][0]["node"]
+            # if the child has ancestor we can remove _node_
+            if ancestor_list:
+                branch_length = tree_cp[node][0]["branch"]
+                parent = ancestor_list[0]
+                siblings = tree_cp[parent]
+                for sibling in siblings:
+                    if sibling['node'] == node:
+                        sibling['node'] = child
+                        sibling['branch'] += branch_length
+                # child ancestor are now exactly node's ancestor
+                merge_unary(child, ancestor_list)
+            # if the child has no grand parent but has children
+            elif tree_cp[tree_cp[node][0]["node"]]:
+                tree_cp[child][0]["branch"] += tree_cp[node][0]["branch"]
+                merge_unary(child, [])
+            tree_cp.pop(node, None)
+        elif len(tree_cp[node]) == 2:
+            merge_unary(tree_cp[node][0]["node"], [node] + ancestor_list)
+            merge_unary(tree_cp[node][1]["node"], [node] + ancestor_list)
+
+    merge_unary(max(tree_cp.keys()), [])
+    tree_rn = {}
+
+    def rename(node):
+        # rename nodes so that all indices are betwen 1 2*n -1
+        children = tree_cp[node]
+        for child in children:
+            child_node = child["node"]
+            child["node"] = sorted(tree_cp.keys()).index(child_node)+1
+            rename(child_node)  # rename child first
+        # now rename actual node
+        node_renamed = sorted(tree_cp.keys()).index(node)+1
+        tree_rn[node_renamed] = tree_cp[node]
+
+    rename(max(tree_cp.keys()))
+    return tree_rn
