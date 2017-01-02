1edb1b1e3a114576edef9252ad7d955bdf3f78b2
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-01-02 14:21:37 +0100

    corrected codon specificity and sensitivity

--- model_evaluation.py
+++ model_evaluation.py
@@ -8,29 +8,14 @@ def translate_states_to_codons(states):
             - states (list or ndarray): the sequence of state
         Output:
             - ndarray: the corresponding codon(1)/non-codon(0) sequence
-        Errors:
-            - ValueError: when the state sequence contains impossible
-            subsequences.
     '''
     codon_sequence = list()
     n_nucleotids = len(states)
     for state_ind, state in enumerate(states):
         if int(state) == 3:
             codon_sequence += [0]
-        if int(state) == 0:
-            if state_ind == n_nucleotids-1:
-                codon_sequence += [1]
-            elif state_ind == n_nucleotids-2:
-                if int(states[-1]) == 1:
-                    codon_sequence += [1]
-                else:
-                    raise ValueError("A 0 state is not followed by 1")
-            else:
-                if (int(states[state_ind+1]) == 1) and\
-                  (int(states[state_ind+2]) == 2):
-                    codon_sequence += [1]
-                else:
-                    raise ValueError("A 0 state is not followed by 1-2")
+        else:
+            codon_sequence += [1]
     return np.array(codon_sequence)
 
 
@@ -45,7 +30,8 @@ def sensitivity(reference_codon_sequence, comparison_codon_sequence):
             - float: the TP rate
     '''
     positive_predictions = comparison_codon_sequence == 1
-    true_predicitons = reference_codon_sequence == comparison_codon_sequence
+    true_predicitons = np.equal(
+        reference_codon_sequence, comparison_codon_sequence)
     true_positives = positive_predictions * true_predicitons
     return np.sum(true_positives) / np.sum(positive_predictions)
 
@@ -61,6 +47,7 @@ def specificity(reference_codon_sequence, comparison_codon_sequence):
             - float: the TN rate
     '''
     negative_predictions = comparison_codon_sequence == 0
-    true_predicitons = reference_codon_sequence == comparison_codon_sequence
+    true_predicitons = np.equal(
+        reference_codon_sequence, comparison_codon_sequence)
     true_negatives = negative_predictions * true_predicitons
     return np.sum(true_negatives) / np.sum(negative_predictions)
